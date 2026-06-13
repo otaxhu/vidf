bdee719f61a9a606a88798430b0963a497733cc1
otaxhu <oscarperniamoreno@gmail.com>
2026-06-13 14:23:41 -0400

    feat: improved test cases, bug with grep guard on _vidf_check_args
    Redirecting stderr to a log file, more test cases, testing _vidf_canonize_args instead of the "_eval" variant
    
    _vidf_check_args wasn't checking multiline malicious user flags, due to using grep, which ignores newlines completely. Changed it to use native shell case-regex syntax. Additionally added a fuzzy test case which tests this regression.

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+: >run-tests.sh.log
+exec 2>>run-tests.sh.log
+
 _VIDF_TEST=1 . ./vidf
+
+_vidf_style_setup 0
 
 # shellcheck disable=SC2016
 _EVAL_EXPECTED_SUCCESS='[ "$?" -ne 0 ] && echo "Expected success, got failure: on line $LINENO" >&2 && exit 1'
@@ -31,6 +36,12 @@ eval "$_EVAL_EXPECTED_FAILURE"
 
 # Missing option, success is expected because they are always optional
 _vidf_check_args "$_TEST_CHECK_ARGS_FORMAT" some-arg -f --bar bar-arg -b b-arg
+eval "$_EVAL_EXPECTED_SUCCESS"
+_vidf_check_args "$_TEST_CHECK_ARGS_FORMAT" some-arg --foo --bar bar-arg -b b-arg
+eval "$_EVAL_EXPECTED_SUCCESS"
+_vidf_check_args "$_TEST_CHECK_ARGS_FORMAT" some-arg --foo -f -b b-arg
+eval "$_EVAL_EXPECTED_SUCCESS"
+_vidf_check_args "$_TEST_CHECK_ARGS_FORMAT" some-arg --foo -f --bar bar-arg
 eval "$_EVAL_EXPECTED_SUCCESS"
 
 # Missing value for option
@@ -108,33 +119,109 @@ _vidf_check_args "$_TEST_CHECK_ARGS_FORMAT" --foo -f --bar bar-arg -b b-arg "$@"
 eval "$_EVAL_EXPECTED_SUCCESS"
 set --
 
-# - _vidf_canonize_args_eval
+# Aliased flags
+
+_TEST_CHECK_ARGS_FORMAT="0,--foo/-f,--bar/-b:"
+
+# Success
+_vidf_check_args "$_TEST_CHECK_ARGS_FORMAT" --foo -f --bar bar-arg -b b-arg
+eval "$_EVAL_EXPECTED_SUCCESS"
+
+# Missing option, success is expected because they are always optional
+_vidf_check_args "$_TEST_CHECK_ARGS_FORMAT" -f --bar bar-arg -b b-arg
+eval "$_EVAL_EXPECTED_SUCCESS"
+_vidf_check_args "$_TEST_CHECK_ARGS_FORMAT" --foo --bar bar-arg -b b-arg
+eval "$_EVAL_EXPECTED_SUCCESS"
+_vidf_check_args "$_TEST_CHECK_ARGS_FORMAT" --foo -f -b b-arg
+eval "$_EVAL_EXPECTED_SUCCESS"
+_vidf_check_args "$_TEST_CHECK_ARGS_FORMAT" --foo -f --bar bar-arg
+eval "$_EVAL_EXPECTED_SUCCESS"
+
+# Missing value for option
+_vidf_check_args "$_TEST_CHECK_ARGS_FORMAT" --foo -f --bar bar-arg -b # Missing here
+eval "$_EVAL_EXPECTED_FAILURE"
+_vidf_check_args "$_TEST_CHECK_ARGS_FORMAT" --foo -f -b b-arg --bar # Missing here
+eval "$_EVAL_EXPECTED_FAILURE"
+
+# Fuzzy cases
+_vidf_err "-------------------"
+_vidf_err "Begin Fuzzy Testing for _vidf_check_args"
+fail_fuzzy=0
+for c in $(seq 1 255)
+do
+  fuzzy_payload="$(awk </dev/null "BEGIN { printf \"--foo%c-suffix\", $c }")"
+  fuzzy_payload="${fuzzy_payload%"-suffix"}"
+
+  if _vidf_check_args "0,--foo" "$fuzzy_payload"
+  then
+    _vidf_err "Expected failure, got success for $c byte"
+    fail_fuzzy=1
+  fi
+done
+
+_vidf_err "End Fuzzy Testing for _vidf_check_args"
+_vidf_err "-------------------"
+
+[ "$fail_fuzzy" = 1 ]
+eval "$_EVAL_EXPECTED_FAILURE"
+
+# - _vidf_canonize_args
 
 _TEST_CANON_ARGS_FORMAT="--foo,-f,--bar:,-b:"
 
-# TODO: Check returned eval args are correct.
+# TODO: Check returned args are correct.
+exec 3>&1 >/dev/null
 
 # Success (already canon)
-_CANON_ARGS="$(_vidf_canonize_args_eval "$_TEST_CANON_ARGS_FORMAT" --foo -f --bar bar-arg -b b-arg)"
+_vidf_canonize_args "$_TEST_CANON_ARGS_FORMAT" --foo -f --bar bar-arg -b b-arg
 eval "$_EVAL_EXPECTED_SUCCESS"
 
 # Success (non-canon)
-_CANON_ARGS="$(_vidf_canonize_args_eval "$_TEST_CANON_ARGS_FORMAT" --foo -fb b-arg --bar=bar-arg)"
+_vidf_canonize_args "$_TEST_CANON_ARGS_FORMAT" --foo -fb b-arg --bar=bar-arg
 eval "$_EVAL_EXPECTED_SUCCESS"
 
 # Success, with arguments.
 #
 # This function doesn't check if the arguments are expected by caller, it just
 # canonize everything.
-_CANON_ARGS="$(_vidf_canonize_args_eval "$_TEST_CANON_ARGS_FORMAT" --foo -f --bar bar-arg -b b-arg some-args other-one)"
+_vidf_canonize_args "$_TEST_CANON_ARGS_FORMAT" --foo -f --bar bar-arg -b b-arg some-args other-one
 eval "$_EVAL_EXPECTED_SUCCESS"
 
 # Missing option, success is expected because they are always optional.
-_CANON_ARGS="$(_vidf_canonize_args_eval "$_TEST_CANON_ARGS_FORMAT" -f --bar bar-arg -b b-arg)"
+_vidf_canonize_args "$_TEST_CANON_ARGS_FORMAT" -f --bar bar-arg -b b-arg
 eval "$_EVAL_EXPECTED_SUCCESS"
 
 # Missing value for option
-_CANON_ARGS="$(_vidf_canonize_args_eval "$_TEST_CANON_ARGS_FORMAT" --foo -f --bar bar-arg -b)" # Missing value on -b
+_vidf_canonize_args "$_TEST_CANON_ARGS_FORMAT" --foo -f --bar bar-arg -b # Missing value on -b
 eval "$_EVAL_EXPECTED_FAILURE"
+
+# Aliased flags
+
+_TEST_CANON_ARGS_FORMAT="--foo/-f,--bar/-b:"
+
+# Success (already canon)
+_vidf_canonize_args "$_TEST_CANON_ARGS_FORMAT" --foo -f --bar bar-arg -b b-arg
+eval "$_EVAL_EXPECTED_SUCCESS"
+
+# Success (non-canon)
+_vidf_canonize_args "$_TEST_CANON_ARGS_FORMAT" --foo -fb b-arg --bar=bar-arg
+eval "$_EVAL_EXPECTED_SUCCESS"
+
+# Success, with arguments.
+#
+# This function doesn't check if the arguments are expected by caller, it just
+# canonize everything.
+_vidf_canonize_args "$_TEST_CANON_ARGS_FORMAT" --foo -f --bar bar-arg -b b-arg some-args other-one
+eval "$_EVAL_EXPECTED_SUCCESS"
+
+# Missing option, success is expected because they are always optional.
+_vidf_canonize_args "$_TEST_CANON_ARGS_FORMAT" -f --bar bar-arg -b b-arg
+eval "$_EVAL_EXPECTED_SUCCESS"
+
+# Missing value for option
+_vidf_canonize_args "$_TEST_CANON_ARGS_FORMAT" --foo -f --bar bar-arg -b # Missing value on -b
+eval "$_EVAL_EXPECTED_FAILURE"
+
+exec >&3 3>&-
 
 exit 0
