684663ad07a3531069ae0d2fb3ebc27f17620b95
otaxhu <oscarperniamoreno@gmail.com>
2026-08-06 18:23:25 -0400

    fix(lib): added license header to vidf-lib.py
    As well as ordering the import statements, following PEP 8 conventions and ordering lexicographically each module import

--- vidf-lib.py
+++ vidf-lib.py
@@ -1,19 +1,34 @@
+# Copyright (C) 2026 Oscar Pernia
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import argparse
-import aiofiles
 import asyncio
-from datetime import datetime, timezone
 import functools
-import httpx2
 import os
 import pathlib
 import re
 import signal
 import sys
 from dataclasses import dataclass
+from datetime import datetime, timezone
 from http import HTTPStatus
 from typing import List, Optional, TextIO, Self
 from urllib.parse import urlsplit, unquote as urlunquote
 
+import aiofiles
+import httpx2
 import rich.markup
 import rich.highlighter
 import rich.theme
